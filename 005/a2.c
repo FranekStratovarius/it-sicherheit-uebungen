fa05ce56424658cd8a0d9918c75f76b115251791
FranekStratovarius <louishuss2000@gmail.com>
2023-11-14 14:10:32 +0000

    aufgabe 5.2

--- 005/a2.c
+++ 005/a2.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 void evil() {
-    printf("i am evil\n");
+	printf("i am evil\n");
 }
 
 void q() {
-    // bitte modifizieren Sie nur im Bereich unterhalb dieser Zeile
+	// bitte modifizieren Sie nur im Bereich unterhalb dieser Zeile
     
-    // Toben Sie sich hier aus!
+	// Toben Sie sich hier aus!
+	int evil_int = 0;
+	*(&evil_int + 3) = (int)&evil;
     
-    // bitte modifizieren Sie nur im Bereich oberhalb dieser Zeile
-    printf("q is done \n");
+	// bitte modifizieren Sie nur im Bereich oberhalb dieser Zeile
+	printf("q is done \n");
 }
 
 void harmless() {
-    q();
+	q();
 }
 
 int main() {
-    harmless();
-    return 0;
+	harmless();
+	return 0;
 }
--- 005/xmake.lua
+++ 005/xmake.lua
@@ -1,6 +1,11 @@
 add_rules("mode.debug", "mode.release")
  
-target("uebung_003")
+target("uebung_005_1")
 	set_kind("binary")
 	add_files("a1.c")
+	add_cflags("-fno-stack-protector")
+
+target("uebung_005_2")
+	set_kind("binary")
+	add_files("a2.c")
 	add_cflags("-fno-stack-protector")
